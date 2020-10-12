6700e0c1aabb262c82c4125546076b4f532cd48f
sulo1337 <s.acharya1337@gmail.com>
2020-10-11 23:53:52 -0500

    allowed cors to all

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
